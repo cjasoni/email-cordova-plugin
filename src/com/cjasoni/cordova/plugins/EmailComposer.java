25c9e9c8136e07f8b955d3d6a9601f9cdfe07311
cjasoni <cjasoni@gmail.com>
2014-10-31 13:40:31 +0800

    update package

--- src/com/cjasoni/cordova/plugins/EmailComposer.java
+++ src/com/cjasoni/cordova/plugins/EmailComposer.java
@@ -1,20 +1,64 @@
 package com.cjasoni.cordova.plugins;
 
-//import org.apache.cordova.api.Plugin;
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.api.PluginResult;
-import org.apache.cordova.api.PluginResult.Status;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.text.Html;
-import com.cjasoni.cordova.utility.Mail;
+
+import com.cjasoni.cordova.plugins.Mail;
  
-@SuppressLint("ParserError")
-public class EmailComposer extends Plugin {
+//@SuppressLint("ParserError")
+public class EmailComposer extends CordovaPlugin {
     
     public final String ACTION_SEND_EMAIL = "sendEmail";
+    
+    
+     
+    @Override
+    public boolean execute(String action, JSONArray arg1, CallbackContext callbackContext) {
+        //PluginResult result = new PluginResult(Status.INVALID_ACTION);
+        if (action.equals(ACTION_SEND_EMAIL)) {
+            try {
+                String message = arg1.getString(0); 
+                String emailTo = arg1.getString(1); 
+                this.sendEmailViaGmail(message,emailTo);
+                callbackContext.success();
+                return true;
+            }
+            catch (JSONException ex) {
+                callbackContext.error(ex.getMessage());
+                return false;
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private void sendEmailViaGmail(String body, String emailTo) throws Exception{
+    	
+    	String gmail = "cjasonigdotw@gmail.com";
+    	String password = "gamedealoftheweek";
+    	String subject = "Game Deal of the Week";
+        Mail m = new Mail(gmail,password);
+        String[] toArr = {emailTo};
+        m.set_to(toArr);
+        m.set_from(gmail);
+        m.set_body(body);
+        m.set_subject(subject);
+        boolean sendFlag = m.send();
+
+    }
+
+    /*
+    
+        
 
      @Override
      public PluginResult execute(String action, JSONArray arg1, String callbackId) {
@@ -47,4 +91,5 @@ public class EmailComposer extends Plugin {
 
      }
 
+     */
 }
